89a1268510fe37c15d8f947b2bdfae1efbbdeac0
Asiel Alonso <Asiel Hextiandro>
2020-05-11 01:32:27 -0400

    login

--- src/container/login/index.tsx
+++ src/container/login/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { LoginForm } from '../../component/lodging'
+import axios from 'axios'
 
 const initialValues = {
     email: '',
@@ -8,8 +9,14 @@ const initialValues = {
 
 export const Login = () => {
 
+    const login = async (email: string, pass: string) => {
+        const dir = `https://atg.party/ws-login-user?email=${email}&password=${pass}&device_name=poly`
+        await axios.post(dir).then(res => { console.log(res) })
+    }
+
     const onLogin = (values: any) => {
-        console.log(values)
+        const { email, pass } = values
+        login(email, pass)
     }
 
     return (<LoginForm initialValues={initialValues} onLogin={onLogin} />)
