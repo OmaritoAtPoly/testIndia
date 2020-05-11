301fabca4b6b8028cc21e0ddf561c74835ded76e
Asiel Alonso <Asiel Hextiandro>
2020-05-11 04:30:48 -0400

    creater dataaccses user

---
+++ src/dataaccess/user.ts
@@ -0,0 +1,12 @@
+import axios from "axios"
+import { domainName, loginUserEndPoint } from "../container"
+
+export const login = async (email: string, pass: string) => {
+    const loginApi = `${domainName}${loginUserEndPoint}?email=${email}&password=${pass}&device_name=poly`
+    return await axios.post(loginApi).then(res => {
+        // console.log(res.data)
+        return res.data.user_details.id
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
