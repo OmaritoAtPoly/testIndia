cfb3228edf16c7579062fc9674c40e952a02b3b8
Omarito <44213559+OmaritoAtPoly@users.noreply.github.com>
2020-05-11 13:32:48 -0500

    Feature no image (#6)
    * login
    
    * cretae useFetch hook
    
    * create ArticlceSwitcher component
    
    * create ArticleViewWithImage
    
    * with noImage Componet ready
    
    Co-authored-by: Asiel Alonso <Asiel Hextiandro>

---
+++ src/container/article/ArticleSwitcher.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { ArticleViewWithImage } from '../../component/article/ArticleViewWithImage'
+import { ArticleViewWithNoImage } from './ArticleViewWithNoImage '
+
+interface Props {
+    article: any
+}
+
+export const ArticleSwitcher = ({ article }: Props) => {
+    const articleDetail = article.PostDetail
+    return componentToRender(articleDetail)
+}
+
+const componentToRender = (articleDetail: any) => {
+    if (articleDetail.profileImage !== undefined) {
+        return (
+            <ArticleViewWithImage
+                title={articleDetail.title}
+                description={articleDetail.description[0].data}
+                profileImage={articleDetail.profileImage}
+            />
+        )
+    } else {
+        return <ArticleViewWithNoImage 
+        title={articleDetail.title} 
+        description={articleDetail.description[0].data}
+        article_id={articleDetail.article_id}
+        tags={articleDetail.tags}
+        created_at={articleDetail.created_at}
+        total_upvote = {articleDetail.total_upvote}
+        comment_count={articleDetail.comment_count}
+        />
+    }
+
+}
