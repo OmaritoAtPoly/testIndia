2f00695b94b4e6e63d820cc3ceaddf389648cb7c
Asiel Alonso <Asiel Hextiandro>
2020-05-11 14:50:40 -0400

    set up paper

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { navigator } from './src/route';
+import { theme } from './src/theme';
 
 const App = () => {
   return (
     <>
       <NavigationContainer>
-        {navigator}
+
+        <PaperProvider
+          theme={theme}
+          settings={{
+            icon: props => <MaterialIcons {...props} />,
+          }}
+        >
+          {navigator}
+        </PaperProvider>
+        {/* {navigator} */}
       </NavigationContainer>
     </>
   );
