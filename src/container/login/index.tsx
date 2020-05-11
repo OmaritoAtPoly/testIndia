a99fa4c512334cf8623d247ca20cec0ac09ae341
Asiel Alonso <Asiel Hextiandro>
2020-05-11 02:17:41 -0400

    create article component

--- src/container/login/index.tsx
+++ src/container/login/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { LoginForm } from '../../component/lodging'
+import { LoginForm } from '../../component/login'
 import axios from 'axios'
 
 const initialValues = {
