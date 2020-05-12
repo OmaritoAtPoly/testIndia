a1e6690225aca1ffdb7a7ed3baea1392f0dabd6c
Omar Estrada Diaz <omarito4857@gmail.com>
2020-05-12 04:20:38 -0500

    adding the error screen

---
+++ src/component/Alerts/AlertShow.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Alert, AlertTitle } from '@material-ui/lab'
+
+export const AlertShow = () => (
+        <Alert variant="outlined" severity="error">
+                This is an error alert — check it out!
+</Alert>
+) 
