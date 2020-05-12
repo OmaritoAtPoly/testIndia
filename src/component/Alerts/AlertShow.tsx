f123e20d15b5562020f7400ef7bbb51837bed254
Omar Estrada Diaz <omarito4857@gmail.com>
2020-05-12 04:46:21 -0500

    with asio login feature

--- src/component/Alerts/AlertShow.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Alert, AlertTitle } from '@material-ui/lab'
-
-export const AlertShow = () => (
-        <Alert variant="outlined" severity="error">
-                This is an error alert — check it out!
-</Alert>
-) 
