4720de20da56861723f11f6cb8e3a20e064e09f3
Omar Estrada Diaz <omarito4857@gmail.com>
2020-05-12 19:05:18 -0500

    with Tags component ready

--- src/component/article/ArticlePageView.tsx
+++ src/component/article/ArticlePageView.tsx
@@ -1,25 +1,32 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
-import { Title } from 'react-native-paper'
+import { Subheading, Title, Text, Drawer } from 'react-native-paper'
 import { ArticleImageProfile } from '../../container/article/image'
 import { Description } from '../../container/description'
 import { theme } from '../../theme'
-import { PostedByList } from './posted'
 import { getFakeText } from '../../utils/utils'
+import { PostedByList } from './posted'
+import { Tags } from './tags/Tags'
 
 interface Props {
     article: any
 }
 
+
 export const ArticlePageView = ({ article }: Props) => {
-    const { title, profile_picture, posted_by } = article.PostDetail
+    const { title, profile_picture, posted_by, first_name, last_name, description, tagline } = article.PostDetail
+
     return (
         <ScrollView style={styles.container} >
             <ArticleImageProfile imageUrl={profile_picture} />
             <View style={styles.content} >
-                <Title>{title}</Title>
-                <Description description={getFakeText()} />
+                <Title style={{ fontSize: 25 }}>{title}</Title>
+                <View>
+                    <Subheading style={{ marginLeft: 25 }}>Articule</Subheading>
+                </View>
+                <Description description={description} />
+                <Tags tags={tagline} />
                 <PostedByList postedList={posted_by} />
             </View>
         </ScrollView>
@@ -45,5 +52,17 @@ const styles = StyleSheet.create({
     postedItem: {
         marginTop: 5,
         marginRight: 5
-    }
+    },
+    // name_last: {
+    //     display: 'flex',
+    //     flexDirection: 'row',
+    //     color: 'red',
+        // fontWeight: '900',
+        // fontSize: 111,
+        // marginTop: 20,
+        // marginLeft: 20,
+        // alignItems: 'center'
+    // },
 });
+
+
---
+++ src/component/article/tags/Tags.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Title, Chip } from 'react-native-paper'
+import { View, StyleSheet } from 'react-native'
+import { theme } from '../../../theme'
+
+interface Props {
+        tags: any;
+}
+
+export const Tags = ({ tags }: Props) => {
+
+        // const allTags = tags.split('|').join();   // todo buena solucion
+        const allTags = tags.split(' | ');
+        // const newtags = allTags.join("");
+
+        // const allTags2 = tagline.split(" | ");
+        // const allTags3 = allTags2.split('"\"')
+    console.log((allTags));
+        // const allTags = tags.split('|');
+    return (
+        <View style={styles.container} >
+            
+            <View style={styles.tags} >
+                {/* {allTags.map((tag:any, ind:number) => <Chip key={ind} style={styles.postedItem} mode="outlined" > {tag}</Chip>)} */}
+                {allTags.map((tag:any, ind:number) => <Chip key={ind} style={styles.postedItem} mode="outlined" >{tag}</Chip>)}
+                {/* {allTags.map((tag:any, ind:number) => console.log('etiketaaaaa', tag))} */}
+                </View>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 10
+    },
+    tags: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+    },
+    postedItem: {
+        marginTop: 5,
+        marginRight: 5
+    }
+});
